25868ffb110aaaf52608059c110a2ab4f5b54531
michaeli105984-ship-it <michaelii105984@mcpsva.org>
2026-03-12 10:52:02 -0400

    Implement replay functionality in number guessing game
    Refactored the number guessing game to allow multiple rounds of play with random number generation.

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -5,19 +5,24 @@ Created: <1/23/2026>
 Instructor: Holtslander
 """
 from random import randint
-
-# num = randint(1, 100)
-num = 4
-
-guess = int(input("Guess a number between 1 and 100\n"))
-
-if num== guess:
-    print("Correct!")
-elif num < guess:
-    print("Too low!")
-elif num > guess:
-    print ("Too high!")
+playing = "y"
+while playing == "y":
+    num = randint(1, 100)
+    guess = 0
+    while guess != num:
+        print("Guess a number between 1 and 100.")
+        guess = input()
+        guess = int(guess)
+        if guess < num:
+            print("Too low! Try again.")
+        elif guess > num:
+            print("Too high! Try again.")
+        else:
+            print(f"You got it! The number was {num}.")
+    playing = input("Play again? (y/N)\n").lower()
+print("Thanks for playing!")
 # number should be random and code needs to repeat
+
 
 
 
